45c7a4c7ea7552c8721d605838f419ea07870826
pl017barbosa-pixel <pl017barbosa@gmail.com>
2026-05-29 16:25:53 +0100

    Create get.session.php

---
+++ get.session.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+header('Content-Type: application/json');
+
+echo json_encode([
+    'email' => $_SESSION['email'] ?? null,
+    'role' => $_SESSION['role'] ?? 'user'
+]);
+?>
